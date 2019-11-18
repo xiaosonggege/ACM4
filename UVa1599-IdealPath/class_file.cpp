e8f84f6dbc152ff50e890fe7a577d1c2eb8f2f7e
xiaosonggege <1243049371@qq.com>
2019-11-18 20:07:00 +0800

    y

--- UVa1599-IdealPath/class_file.cpp
+++ UVa1599-IdealPath/class_file.cpp
@@ -8,40 +8,44 @@
 using namespace std;
 graph::graph(const string &paths, const int &points, const int &edges):
 path(paths), point_num(points), edge_num(edges) {
-    ifstream ifstrm;
-    ifstrm.open(path, ios::in);
-    string temp_s;
-    int basic_info_finish = 0;
-    vector<tuple<int, int, int>> info; //存储无向图中的结点间的连接信息
-    while (getline(ifstrm, temp_s)){
-        istringstream istr(temp_s);
-        if (!basic_info_finish) {
-            istr >> this->point_num >> this->edge_num;
-            basic_info_finish = 1 - basic_info_finish;
+    try{
+        ifstream ifstrm;
+        ifstrm.open(path, ios::in);
+        if (!ifstrm) throw string("文件打开失败");
+        string temp_s;
+        int basic_info_finish = 0;
+        vector<tuple<int, int, int>> info; //存储无向图中的结点间的连接信息
+        while (getline(ifstrm, temp_s)){
+            istringstream istr(temp_s);
+            if (!basic_info_finish) {
+                istr >> this->point_num >> this->edge_num;
+                basic_info_finish = 1 - basic_info_finish;
+            }
+            else{
+                int temp_p1, temp_p2, color;
+                istr >> temp_p1 >> temp_p2 >> color;
+                info.push_back(make_tuple(temp_p1, temp_p2, color));
+            }
         }
-        else{
-            int temp_p1, temp_p2, color;
-            istr >> temp_p1 >> temp_p2 >> color;
-            info.push_back(make_tuple(temp_p1, temp_p2, color));
-        }
-    }
 //    for (auto const &e : info){
 //        cout << get<0>(e) << " " << get<1>(e) << " " << get<2>(e) << endl;
 //    }
-    //对linjiebiao进行赋值
-    this->linjiebiao = vector<node>(this->point_num, node());
-    for (int num = 0; num != this->point_num; ++num) this->linjiebiao[num].number = num+1;
-    node_ptr node_move; //建立邻接表中横向移动指针
-    for (auto const &e : info){
-        //判断该结点对是否已经存在，如果存在则直接将颜色加入对应序列中
+        //对linjiebiao进行赋值
+        this->linjiebiao = vector<node>(this->point_num, node());
+        for (int num = 0; num != this->point_num; ++num) this->linjiebiao[num].number = num+1;
+        node_ptr node_move; //建立邻接表中横向移动指针
+        for (auto const &e : info){
+            //判断该结点对是否已经存在，如果存在则直接将颜色加入对应序列中
 //        node_move = &this->linjiebiao[get<0>(e)];
-        if (find_node(get<0>(e), get<1>(e), *this, node_move)) node_move->color_level.push_back(get<2>(e));
-        else node_move->next = new node(get<1>(e), vector<int>({get<2>(e)}));
-        node_move = &this->linjiebiao[get<1>(e)];
-        if (find_node(get<1>(e), get<0>(e), *this, node_move)) node_move->color_level.push_back(get<2>(e));
-        else node_move->next = new node(get<0>(e), vector<int>({get<2>(e)}));
+            if (find_node(get<0>(e), get<1>(e), *this, node_move)) node_move->color_level.push_back(get<2>(e));
+            else node_move->next = new node(get<1>(e), vector<int>({get<2>(e)}));
+            node_move = &this->linjiebiao[get<1>(e)];
+            if (find_node(get<1>(e), get<0>(e), *this, node_move)) node_move->color_level.push_back(get<2>(e));
+            else node_move->next = new node(get<0>(e), vector<int>({get<2>(e)}));
+        }
+        ifstrm.close();
     }
-    ifstrm.close();
+    catch (const string &error) {cerr << error;}
     //初始化dis2end
     int i = 1;
     while (i != this->point_num+1){
@@ -127,5 +131,7 @@ void graph::calc_dis2end() {
         node_p = &this->linjiebiao[stack->front()];
     }
     shared_ptr<vector<int>> ptr_stack(stack), ptr_stack_2(stack_2);
+    stack = nullptr;
+    stack_2 = nullptr;
 }
 
