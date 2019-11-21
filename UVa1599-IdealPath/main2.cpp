b1d6574c6a9e206c7948bff242dbfa0dc92efb17
xiaosonggege <1243049371@qq.com>
2019-11-21 13:15:16 +0800

    y

--- UVa1599-IdealPath/main2.cpp
+++ UVa1599-IdealPath/main2.cpp
@@ -5,13 +5,13 @@
 #include "stdio.h"
 #include "stdlib.h"
 typedef struct BiTNode{
-    int data = 0;
+    char data = ' ';
     struct BiTNode *lchild = NULL;
     struct BiTNode *rchild = NULL; //左右孩子指针
 }BiTNode, *BiTree;
 //建立用于层序遍历队列操作的队列结构
 typedef struct QueueNode{
-    BiTNode data;
+    BiTree data;
     QueueNode *next;
 }QueueNode,*QueueNode_p;
 
@@ -25,7 +25,7 @@ Queue_p init(Queue_p pq){
     pq->rear = pq->front;
     return pq;
 }
-void enqueue(Queue_p pq, BiTNode t){
+void enqueue(Queue_p pq, BiTree t){
     QueueNode_p pNew = (QueueNode_p)malloc(sizeof(QueueNode));
     pNew->data = t;
     pNew->next = NULL;
@@ -41,67 +41,78 @@ QueueNode_p dequeue(Queue_p pq){
     }else{
         pq->front->next = pTemp->next;
     }
-    QueueNode_p x = pTemp;
-    free(pTemp);
-    return x;
+//    QueueNode_p x = pTemp;
+//    free(pTemp);
+    return pTemp;
 }
 ////////
 int main(int argc, char *argv[]){
     ///////////////////////////////////函数初始化区域
-    void visit(int e); //对二叉树结点操作的函数
+    void visit(char e); //对二叉树结点操作的函数
     BiTree & InitBiTree(BiTree &T); //构造空二叉树
     void CreateBiTree(BiTree &T, char string[], int size); //根据要求次序输入二叉树中结点值
-    void PreOrderTraverse(BiTree &T, void (*visit)(int e)); //先序遍历二叉树
-    void InOrderTraverse(BiTree &T, void (*visit)(int e)); //中序遍历二叉树
-    void InOrderTraverse_no_cur_no_stack(BiTree &T, void (*visit)(int e)); //中序非递归无堆栈遍历二叉树
-    void PostOrderTraverse(BiTree &T, void (*visit)(int e)); //后序遍历二叉树
-    void LevelOrderTraverse(BiTree &T, void (*visit)(int e)); //层序遍历二叉树
+    void PreOrderTraverse(BiTree &T, void (*visit)(char e)); //先序遍历二叉树
+    void InOrderTraverse(BiTree &T, void (*visit)(char e)); //中序遍历二叉树
+    void InOrderTraverse_no_cur_no_stack(BiTree &T, void (*visit)(char e)); //中序非递归无堆栈遍历二叉树
+    void PostOrderTraverse(BiTree &T, void (*visit)(char e)); //后序遍历二叉树
+    void LevelOrderTraverse(BiTree &T, void (*visit)(char e)); //层序遍历二叉树
     void DestroyBiTree(BiTree &T); //销毁二叉树
     ///////////////////////////////////
     char input[] = "ABC#D#E";
-    printf("%lu\n", sizeof(input)/sizeof(char));
+//    printf("%lu\n", sizeof(input)/sizeof(char));
     BiTree root = (BiTree)malloc(sizeof(BiTNode));
     CreateBiTree(root, input, sizeof(input)/sizeof(char)-1);
+    printf("先序遍历!");
+    PreOrderTraverse(root, visit);
+    printf("\n");
+    printf("中序遍历!");
+    InOrderTraverse(root, visit);
+    printf("\n");
+    printf("后序遍历!");
+    PostOrderTraverse(root, visit);
+    printf("\n");
+    printf("层序遍历!");
+    LevelOrderTraverse(root, visit);
     DestroyBiTree(root);
     return 0;
 }
-void visit(int e){
-    printf("%d\t", e);
+void visit(char e){
+    printf("%c\t", e);
 }
 BiTree & InitBiTree(BiTree &T){
     if (!(T = (BiTNode *)malloc(sizeof(BiTNode)))) exit(EOVERFLOW);
     return T;
 }
-void PreOrderTraverse(BiTree &T, void (*visit)(int e)){
+void PreOrderTraverse(BiTree &T, void (*visit)(char e)){
     if (T){
         visit(T->data);
         PreOrderTraverse(T->lchild, visit);
         PreOrderTraverse(T->rchild, visit);
     }
 }
-void InOrderTraverse(BiTree &T, void (*visit)(int e)){
+void InOrderTraverse(BiTree &T, void (*visit)(char e)){
     if (T){
         InOrderTraverse(T->lchild, visit);
         visit(T->data);
         InOrderTraverse(T->rchild, visit);
     }
 }
-void PostOrderTraverse(BiTree &T, void (*visit)(int e)){
+void PostOrderTraverse(BiTree &T, void (*visit)(char e)){
     if (T){
         PostOrderTraverse(T->lchild, visit);
         PostOrderTraverse(T->rchild, visit);
         visit(T->data);
     }
 }
-void LevelOrderTraverse(BiTree &T, void (*visit)(int e)){
+void LevelOrderTraverse(BiTree &T, void (*visit)(char e)){
     Queue_p queue = (Queue_p)malloc(sizeof(QueueNode));
     queue = init(queue); //初始化队列
-    enqueue(queue, *T); //根结点入队
+    enqueue(queue, T); //根结点入队
     while (queue->front != queue->rear){
         QueueNode_p queue_front = dequeue(queue);
-        visit(queue_front->data.data);
-        if (queue_front->data.lchild) enqueue(queue, *queue_front->data.lchild);
-        if (queue_front->data.rchild) enqueue(queue, *queue_front->data.rchild);
+        visit(queue_front->data->data);
+        if (queue_front->data->lchild) enqueue(queue, queue_front->data->lchild);
+        if (queue_front->data->rchild) enqueue(queue, queue_front->data->rchild);
     }
 }
 void CreateBiTree(BiTree &T, char string[], int size){
@@ -109,27 +120,27 @@ void CreateBiTree(BiTree &T, char string[], int size){
     queue = init(queue); //初始化队列
     int pos = 0;
     T->data = *(string + (pos++));
-    enqueue(queue, *T); //根结点入队
+    enqueue(queue, T); //根结点入队
     while (queue->front != queue->rear && pos != size){
         QueueNode_p queue_front = dequeue(queue);
         if (pos != size) {
             if (*(string + pos) != '#'){
                 BiTree binode_lchild = (BiTree)malloc(sizeof(BiTNode)); //开辟新的二叉树左孩子结点
                 binode_lchild->data = *(string + pos);
-                queue_front->data.lchild = binode_lchild;
-                enqueue(queue, *binode_lchild);
+                queue_front->data->lchild = binode_lchild;
+                enqueue(queue, binode_lchild);
             }
-            else ++pos;
+            ++pos;
         }
         else break;
         if (pos != size && *(string + pos) != '#'){
             if (*(string + pos) != '#') {
                 BiTree binode_rchild = (BiTree)malloc(sizeof(BiTNode)); //开辟新的二叉树右孩子结点
                 binode_rchild->data = *(string + pos);
-                queue_front->data.rchild = binode_rchild;
-                enqueue(queue, *binode_rchild);
+                queue_front->data->rchild = binode_rchild;
+                enqueue(queue, binode_rchild);
             }
-            else ++pos;
+            ++pos;
         }
         else break;
     }
