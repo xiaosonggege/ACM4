09b47e7c61f535a73d07605eeaeebbbd24f751b2
xiaosonggege <1243049371@qq.com>
2019-11-26 17:10:39 +0800

    y

---
+++ UVa11853-Paintball/CMakeFiles/UVa11853.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/UVa11853.dir/main.cpp.o"
+  "CMakeFiles/UVa11853.dir/Paintball.cpp.o"
+  "UVa11853.pdb"
+  "UVa11853"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/UVa11853.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
