21e0f133d83bd000b98e99ad273980880bb74c9b
xiaosonggege <1243049371@qq.com>
2019-12-01 11:17:11 +0800

    y

---
+++ UVa712-STree/CMakeFiles/UVa712.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/UVa712.dir/main.cpp.o"
+  "CMakeFiles/UVa712.dir/stree.cpp.o"
+  "UVa712"
+  "UVa712.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/UVa712.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- UVa712-STree/main.cpp
+++ UVa712-STree/main.cpp
@@ -6,13 +6,18 @@
 #include <vector>
 #include <fstream>
 #include <string>
+#include <fstream>
+#include <sstream>
+#include <memory>
 using namespace std;
 int main(int argc, char *argv[]){
+    cout << argv[1] << endl;
     const string path = "/Users/songyunlong/CLionProjects/ACM4/UVa712-STree/file";
     patrolli p(3, "00000111", vector<string>({"000", "010", "111", "110"})),
             p2(3, "00010011", vector<string>({"000", "010", "111", "110"}));
 //    p(cout);
     p2.go();
     p2(cout);
+
     return 0;
 }
